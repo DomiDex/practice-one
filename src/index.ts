d45fb92281a4ef17df7de2d30c2e7bc0f6ed095e
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2024-12-11 20:52:45 +0700

    Add Fibonacci sequence function to index.ts

--- src/index.ts
+++ src/index.ts
@@ -285,3 +285,14 @@ console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])); //[1, 2, 3, 4, 5]
 console.log(removeDuplicates([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); //[1]
 console.log(removeDuplicates([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10])); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 10])); //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//FibonacciSequence
+function fibonacciSequence(n: number): number[] {
+  let fibonacci: number[] = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+  return fibonacci;
+}
+
+console.log(fibonacciSequence(10)); //[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+console.log(fibonacciSequence(20)); //[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
